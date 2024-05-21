e609a05a5990eca89aeb6787e2a161a1b3c61093
Gabrielarndt <gabrielharndt@hotmail.com>
2024-05-20 23:09:26 -0300

    up

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,29 @@
-// webpack.config.js
+const path = require('path');
 
 module.exports = {
-    // outras configurações do webpack...
-  
+    entry: './src/index.js', // Arquivo de entrada principal
+    output: {
+        path: path.resolve(__dirname, 'dist'), // Diretório de saída para os arquivos construídos
+        filename: 'bundle.js' // Nome do arquivo de saída
+    },
+    module: {
+        rules: [
+            // Adicione regras para carregar diferentes tipos de arquivos, como JavaScript, CSS, etc.
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader' // Carrega arquivos JavaScript usando Babel
+                }
+            }
+        ]
+    },
     resolve: {
-      fallback: {
-        "path": require.resolve("path-browserify")
-      }
+        fallback: {
+          "path": require.resolve("path-browserify")
+        }
+      },    
+    "scripts": {
+        "build": "webpack --config webpack.config.js"
     }
-  };
-  
+};
